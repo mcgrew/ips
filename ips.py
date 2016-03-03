f013ce7df788a0cd8b09faba75b6d5cbfb1f0be0
Thomas McGrew <tjmcgrew@gmail.com>
2016-03-03 13:46:06 -0500

    Renamed some variables

--- ips.py
+++ ips.py
@@ -3,86 +3,86 @@
 import struct
 import argparse
 
-def create_ips(file1_data, file2_data):
-  return Patch.create(file1_data, file2_data).encode()
+def create_ips(file1_content, file2_content):
+  return Patch.create(file1_content, file2_content).encode()
 
-def apply_ips(file_data, patch_data):
-  patch = Patch(patch_data)
-  return patch.apply(file_data)
+def apply_ips(file_content, patch_content):
+  patch = Patch(patch_content)
+  return patch.apply(file_content)
 
 class Patch:
   records = [] 
-  def __init__(self, ips_data=None):
-    if ips_data and ips_data[:5] == 'PATCH':
+  def __init__(self, ips_content=None):
+    if ips_content and ips_content[:5] == 'PATCH':
       ips_ptr = 5
-      file_size = len(file_data)
+      file_size = len(file_content)
       # read and apply the patches
-      while (ips_ptr < len(ips_data)):
-        record_meta = struct.unpack(">BHH", ips_data, ips_ptr)
+      while (ips_ptr < len(ips_content)):
+        record_meta = struct.unpack(">BHH", ips_content, ips_ptr)
         ips_ptr += 5
         record_addr = record_meta[0] << 16 | record_meta[1]
         record_size = record_meta[2]
         if record_size:
-          record_data = struct.unpack("B" * record_size, ips_data, ips_ptr)
+          record_content = struct.unpack("B" * record_size, ips_content, ips_ptr)
           ips_ptr += record_size
         else: #run length encoded
-          record_size = struct.unpack("B", ips_data, ips_ptr)
+          record_size = struct.unpack("B", ips_content, ips_ptr)
           ips_ptr += 2
-          record_data = struct.unpack("B", ips_data, ips_ptr) * record_size
+          record_content = struct.unpack("B", ips_content, ips_ptr) * record_size
           ips_ptr += 1
-        records.append(Record(record_addr, record_data))
+        records.append(Record(record_addr, record_content))
     
-  def apply(self, orig_data):
-    orig_data = bytearray(orig_data)
+  def apply(self, orig_content):
+    orig_content = bytearray(orig_content)
     for record in self.records:
-      orig_data[record.address:record.address+record.size()] = record.data
-    return orig_data
+      orig_content[record.address:record.address+record.size()] = record.content
+    return orig_content
 
   def encode(self):
     encoded =  b''.join([r.encode() for r in self.records])
     return b''.join((b'PATCH', encoded, b'EOL'))
 
-  def add_record(self, address, data):
-    self.records.append(Record(address, data))
+  def add_record(self, address, content):
+    self.records.append(Record(address, content))
 
   @staticmethod
-  def create(orig_data, patched_data):
+  def create(orig_content, patched_content):
     p = Patch()
-    if not len(orig_data) == len(patched_data):
+    if not len(orig_content) == len(patched_content):
       raise ValueError("File sizes do not match.")
     diff_addr = -1
     last_diff = -1
-    for i in range(len(orig_data)):
-      if not orig_data[i] == patched_data[i]:
+    for i in range(len(orig_content)):
+      if not orig_content[i] == patched_content[i]:
         if diff_addr < 0:
           diff_addr = i
         last_diff = i
       if last_diff >= 0 and i - last_diff >= 5:
-        p.add_record(diff_addr, patched_data[diff_addr:last_diff+1])
+        p.add_record(diff_addr, patched_content[diff_addr:last_diff+1])
         diff_addr = -1
         last_diff = -1
     return p
 
 class Record:
-  def __init__(self, address, data=None):
+  def __init__(self, address, content=None):
     self.address = address 
-    self.data = data
+    self.content = content
 
   def set_addr(self, addr):
     self.address = addr
 
-  def set_data(self, data):
-    self.data = data
+  def set_content(self, content):
+    self.content = content
 
   def size(self):
-    if self.data:
-      return len(self.data)
+    if self.content:
+      return len(self.content)
     else:
       return 0
 
   def encode(self):
     return struct.pack('>BHH' + 'B' * self.size(), self.address >> 16, 
-        self.address & 0xff, self.size(), *[int(b) for b in self.data])
+        self.address & 0xff, self.size(), *[int(b) for b in self.content])
     
 
 def main():
@@ -94,27 +94,27 @@ def main():
   parser.add_argument("file2", help="The second input file")
   args = parser.parse_args()
 
-  patch_data = None
+  patch_content = None
 
   file1 = open(args.file1, 'rb')
-  file1_data = file1.read()
+  file1_content = file1.read()
   file1.close()
   file2 = open(args.file2, 'rb')
-  file2_data = file2.read()
+  file2_content = file2.read()
   file2.close()
 
-  if file1_data[:5] == b'PATCH':
-    patch_data = file1_data
-    file1_data = file2_data
-    file2_data = None
-  elif file2_data[:5] == b'PATCH':
-    patch_data = file2_data
-    file2_data = None
+  if file1_content[:5] == b'PATCH':
+    patch_content = file1_content
+    file1_content = file2_content
+    file2_content = None
+  elif file2_content[:5] == b'PATCH':
+    patch_content = file2_content
+    file2_content = None
   
-  if patch_data:
-    out = apply_ips(file1_data, patch_data)
+  if patch_content:
+    out = apply_ips(file1_content, patch_content)
   else:
-    out = create_ips(file1_data, file2_data)
+    out = create_ips(file1_content, file2_content)
   
   outfile = open(args.output, 'wb')
   outfile.write(out)
